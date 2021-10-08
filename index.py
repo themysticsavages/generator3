901b6b7b2322d01db25c28d38869e3b6aaa62d66
ajskateboarder <81255936+themysticsavages@users.noreply.github.com>
2021-10-08 10:30:32 -0400

    Add files via upload

--- index.py
+++ index.py
@@ -17,14 +17,14 @@ class util():
       fh.write('{}\n'.format(targets[i]['name']))
 
       ht.write("""
-                <!doctype html>
-                <html>
-                  <head>
-                    <script src="https://scratchblocks.github.io/js/scratchblocks-v3.5.2-min.js"></script>
-                  <head>
-                  <body>
-                    <p class="blocks">
-              """)
+      <!doctype html>
+      <html>
+        <head>
+          <script src="https://scratchblocks.github.io/js/scratchblocks-v3.5.2-min.js"></script>
+        </head>
+        <body>
+          <p class="blocks">
+      """)
 
       for k in targets[i]['blocks']:
         opcode = targets[i]['blocks'][k]['opcode']
@@ -32,13 +32,22 @@ class util():
         fnd = targets[i]
         let = ''
 
+        print(opcode)
         if opcode == 'event_whenflagclicked':
           let += 'when flag clicked:'
+        if opcode == 'event_whenbroadcastreceived':
+          br = root['fields']['BROADCAST_OPTION'][0]
+          let += 'when i receive [{}]'.format(br)
         if opcode == 'looks_seteffectto':
           ef = root['fields']['EFFECT'][0]
           num = root['inputs']['VALUE'][1][1]
 
           let += 'set [{}] effect to ({})'.format(ef.lower(),num)
+        if opcode == 'control_create_clone_of':
+          mn = root['inputs']['CLONE_OPTION'][1]
+          opt = targets[i]['blocks'][mn]['fields']['CLONE_OPTION'][0].replace('_','')
+          
+          let += 'create clone of [{}]'.format(opt)
         if opcode == 'motion_gotoxy':
           try:
             x = root['inputs']['X'][2][0]
@@ -47,11 +56,11 @@ class util():
             x = root['inputs']['X'][1][0]
             y = root['inputs']['Y'][1][1]
           
-          let += 'go to x: ({}) y: ({})'.format(x,y)
+          let += 'go to x ({}) y ({})'.format(x,y)
         if opcode == 'motion_changeyby':
           y = root['inputs']['DY'][2][1]
 
-          let += 'change y by: ({})'.format(y)
+          let += 'change y by ({})'.format(y)
         if opcode == 'motion_glidesecstoxy':
           secs = root['inputs']['SECS'][1][1]
           
@@ -62,12 +71,12 @@ class util():
             x = root['inputs']['X'][1][1]
             y = root['inputs']['Y'][1][1]
           
-          let += 'glide ({}) secs to x: ({}) y: ({})'.format(secs,x,y)
+          let += 'glide ({}) secs to x ({}) y ({})'.format(secs,x,y)
         if opcode == 'looks_switchcostumeto':
           cos = root['inputs']['COSTUME'][1]
 
           try:
-            let += 'switch costume to: [{}]'.format(targets[i]['blocks'][cos]['fields']['COSTUME'][0])
+            let += 'switch costume to [{}]'.format(targets[i]['blocks'][cos]['fields']['COSTUME'][0])
           except KeyError:
             let += 'switch costume to (not found)'
         if opcode == 'looks_changeeffectby':
@@ -79,18 +88,28 @@ class util():
           let += 'show '
         if opcode == 'looks_hide':
           let += 'hide '
+        if opcode == 'data_setvariableto':
+          var = root['fields']['VARIABLE'][0]
+          val = root['inputs']['VALUE'][1][1]
+
+          let += 'set [{}] to [{}]'.format(var,val)
+        if opcode == 'sound_playuntildone':
+          d = root['inputs']['SOUND_MENU'][1]
+          val = targets[i]['blocks'][d]['fields']['SOUND_MENU'][0]
+
+          let += 'play sound [{}] until done'.format(val)
 
         fh.write(let+'\n')
         ht.write(let+'\n')
 
       ht.write("""
-                </p>
-                <script type="text/javascript">
-                  scratchblocks.renderMatching("p.blocks", {
-                    style: 'scratch3'
-                  })
-                </script>
-              """)
+      </p>
+      <script type="text/javascript">
+      scratchblocks.renderMatching("p.blocks", {
+          style: 'scratch3'
+      })
+      </script>
+      """)
 
   def serveHTML():
     app = flask.Flask('__main__')
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import index
 import time
 import os
+
 try: 
   os.remove('results.txt') 
 except FileNotFoundError: pass
+
+for item in os.listdir('./'):
+    if item.endswith(".html"):
+        os.remove(os.path.join('./', item))
 
 inp = input('[LOG] Enter a project ID > ')
 
@@ -13,5 +18,5 @@ ds = int(time.time()*1000)
 index.util(inp).getScripts()
 
 print('[LOG] Finished in {}ms'.format(int(time.time()*1000-ds)))
-
+print('[LOG] Starting temporary webserver ...')
 index.util.serveHTML()
